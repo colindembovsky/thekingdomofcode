d89457ee5b66131fc17602859e2a44c41e382c4c
Colin Dembovsky <colindembovsky@github.com>
2025-02-24 10:56:17 -0600

    jekylize

---
+++ _site/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.html',
+    './_posts/*.{html,md}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
